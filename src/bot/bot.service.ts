f92a6acffd0c91b632740259e0196120f084ed3c
danlaptev <danlaptev@yandex-team.ru>
2023-04-17 09:27:55 +0300

    исправить формат ответа createInvoiceLink

--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -6,8 +6,10 @@ const rubToCents = (rub: number) => rub * 100;
 
 @Injectable()
 export class BotService {
-  async createInvoiceLink({ prices }: CreateInvoiceLinkDto): Promise<string> {
-    return await bot.telegram.createInvoiceLink({
+  async createInvoiceLink({
+    prices,
+  }: CreateInvoiceLinkDto): Promise<{ link: string }> {
+    const link = await bot.telegram.createInvoiceLink({
       title: 'Заказ',
       description: 'Описание',
       provider_token: process.env.YO_KASSA_TEST_TOKEN,
@@ -18,5 +20,7 @@ export class BotService {
         amount: rubToCents(price.amount),
       })),
     });
+
+    return { link };
   }
 }
